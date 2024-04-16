0c0e335fa28958ad07b1b295df78f00691075a51
chococochiffon <66964566+chococochiffon@users.noreply.github.com>
2024-04-16 17:58:45 +0900

    変更

--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
+  <div>
+    <section class="py-5 bg-body-tertiary">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
 
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
