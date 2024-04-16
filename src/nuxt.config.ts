e9ae5b44dfc789d85322940850825c9904dcdab7
chococochiffon <chococo.chiffon@gmail.com>
2024-04-17 03:04:29 +0900

    日本語用のフォントを入れた

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -11,7 +11,14 @@ export default defineNuxtConfig({
         { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js' }
       ],
       link: [
-        { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css' },
+        // { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css' },
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: "" },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap' },
+
+        // <link rel="preconnect" href="https://fonts.googleapis.com">
+        // <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        // <link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
       ]
     }
   },
--- src/pages/profile/index.vue
+++ src/pages/profile/index.vue
@@ -4,10 +4,92 @@
 
 <template>
   <div>
-    <section class="py-5 bg-body-tertiary">
-      <div class="container">
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <section style="background-color: #eee;">
+      <div class="container py-5">
 
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card mb-4">
+              <div class="card-body text-center">
+                <img src="@/assets/image/profile/IMG_4363.png" alt="avatar"
+                     class="rounded-circle img-fluid" style="width: 150px;">
+                <h5 class="my-3">Chococo Chiffon</h5>
+                <p class="text-muted mb-1">Full Stack Developer &amp; Gamer</p>
+                <p class="text-muted mb-4"></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="card mb-4">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-sm-3">
+                    <p class="mb-0">なまえ</p>
+                  </div>
+                  <div class="col-sm-9">
+                    <p class="text-muted mb-0">Chococo Chiffon</p>
+                  </div>
+                </div>
+                <hr>
+                <div class="row">
+                  <div class="col-sm-3">
+                    <p class="mb-0">Email</p>
+                  </div>
+                  <div class="col-sm-9">
+                    <p class="text-muted mb-0">chococo.chiffon@gmail.com</p>
+                  </div>
+                </div>
+                <hr>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="card mb-4 mb-md-0">
+                  <div class="card-body">
+                    <h5 class="card-title">自己紹介</h5>
+                    <p class="text-muted mb-2">
+                      ゲーム大好きプログラマー。<br/>
+                      フリーランスとして活動しています。<br/>
+                      動画製作に興味があります。
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="card mb-4 mb-md-0">
+                  <div class="card-body">
+                    <p class="mb-4"><span class="text-primary font-italic me-1">Chococo Chiffon</span>Status
+                    </p>
+                    <p class="mb-1" style="font-size: .77rem;">Web Design</p>
+                    <div class="progress rounded" style="height: 5px;">
+                      <div class="progress-bar" role="progressbar" style="width: 40%" aria-valuenow="40"
+                           aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mt-4 mb-1" style="font-size: .77rem;">Frontend</p>
+                    <div class="progress rounded mb-2" style="height: 5px;">
+                      <div class="progress-bar" role="progressbar" style="width: 74%" aria-valuenow="74"
+                           aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mt-4 mb-1" style="font-size: .77rem;">Backend API</p>
+                    <div class="progress rounded mb-2" style="height: 5px;">
+                      <div class="progress-bar" role="progressbar" style="width: 98%" aria-valuenow="98"
+                           aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mt-4 mb-1" style="font-size: .77rem;">Video Creator</p>
+                    <div class="progress rounded mb-2" style="height: 5px;">
+                      <div class="progress-bar" role="progressbar" style="width: 0%" aria-valuenow="0"
+                           aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <p class="mt-4 mb-1" style="font-size: .77rem;">Games</p>
+                    <div class="progress rounded mb-2" style="height: 5px;">
+                      <div class="progress-bar" role="progressbar" style="width: 20%" aria-valuenow="20"
+                           aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
